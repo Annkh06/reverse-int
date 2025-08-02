93e42931701af8853ee9d0e6dc661bb5b3f11007
Anna Khodosovskaya <hodosovskaaanna@gmail.com>
2025-08-03 01:58:00 +0300

    feat: done the exercise reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  if (n > 0) {
+    return String(n).split('').reverse().join('') * Math.sign(n);
+  }
+  if (n < 0) {
+    return String(-n).split('').reverse().join('');
+  }
+  return n;
 };
